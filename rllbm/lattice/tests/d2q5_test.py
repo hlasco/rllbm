2f40733829a2f6192a9463ffbe490e92d84e4adf
hlasco <hugues.lascombesdelaroussilhe.external@atos.net>
2023-03-21 11:33:45 +0100

    Update tests

--- rllbm/lattice/tests/d2q5_test.py
+++ rllbm/lattice/tests/d2q5_test.py
@@ -2,17 +2,15 @@ from rllbm.lattice import D2Q5
 import jax.numpy as jnp
 
 def test_D2Q5_moment_0():
-    l = D2Q5()
     df = jnp.ones((16,16,5))
-    m0 = l.get_moment(df, order=0)
+    m0 = D2Q5.get_moment(df, order=0)
     
     expected = 5
     assert (m0 == expected).all()
     
 def test_D2Q5_moment_1():
-    l = D2Q5()
     df = jnp.ones((16,16,5))
-    m1 = l.get_moment(df, order=1)
+    m1 = D2Q5.get_moment(df, order=1)
     
     expected = jnp.array(
         [0., 0.]
@@ -20,9 +18,8 @@ def test_D2Q5_moment_1():
     assert (m1 == expected).all()
     
 def test_D2Q5_moment_2():
-    l = D2Q5()
     df = jnp.ones((16,16,5))
-    m2 = l.get_moment(df, order=2)
+    m2 = D2Q5.get_moment(df, order=2)
     expected = jnp.array(
         [
             [2., 0.],
--- rllbm/lattice/tests/d2q9_test.py
+++ rllbm/lattice/tests/d2q9_test.py
@@ -2,17 +2,15 @@ from rllbm.lattice import D2Q9
 import jax.numpy as jnp
     
 def test_D2Q9_moment_0():
-    l = D2Q9()
     df = jnp.ones((16,16,9))
-    m0 = l.get_moment(df, order=0)
+    m0 = D2Q9.get_moment(df, order=0)
     
     expected = 9
     assert (m0 == expected).all()
     
 def test_D2Q9_moment_1():
-    l = D2Q9()
     df = jnp.ones((16,16,9))
-    m1 = l.get_moment(df, order=1)
+    m1 = D2Q9.get_moment(df, order=1)
 
     expected = jnp.array(
         [0., 0.]
@@ -20,9 +18,8 @@ def test_D2Q9_moment_1():
     assert (m1 == expected).all()
     
 def test_D2Q9_moment_2():
-    l = D2Q9()
     df = jnp.ones((16,16,9))
-    m2 = l.get_moment(df, order=2)
+    m2 = D2Q9.get_moment(df, order=2)
     
     expected = jnp.array(
         [
@@ -32,5 +29,3 @@ def test_D2Q9_moment_2():
     )
 
     assert (m2 == expected).all()
-
-test_D2Q9_moment_2()
